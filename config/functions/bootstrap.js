f9e400f6d1af3d8226ffbab4c9b255b8068f54af
Will Alvarez <will.alvarez@gmail.com>
2020-06-12 12:41:00 -0500

    remove local packages and sockets

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,18 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+    process.nextTick(() =>{
+      var io = require('socket.io')(strapi.server);
+      io.on('connection', function(socket){
+        console.log("a user connected")
+        // send message on user connection
+        socket.emit('hello', JSON.stringify({message: 'Hello user'}));
+
+
+        // listen for user diconnect
+        socket.on('disconnect', () => console.log('a user disconnected'));
+      });
+      strapi.io = io; // register socket io inside strapi main object to use it globally anywhere
+    })
+};
