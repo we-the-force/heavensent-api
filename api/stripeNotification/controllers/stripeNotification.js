77ea1fee77352489372b3ce6de988e9d2d44eca4
Iojioji <erfamel@hotmail.com>
2020-08-10 17:47:08 -0500

    return to heavensent email

--- api/stripeNotification/controllers/stripeNotification.js
+++ api/stripeNotification/controllers/stripeNotification.js
@@ -563,7 +563,7 @@ async function sendPaymentSucceedEmail(customerEmail, entry)
     </html>`
         strapi.plugins['email'].services.email.send({
             to: customerEmail,
-            from: 'about@wetheforce.com',
+            from: 'info@heavensentnow.com',
             subject: 'HeavenSent membership charge',
             html: htmlEmail
         }).catch(function (promErr){
