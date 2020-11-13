fe956d6b67f93a8b4cb9fb13ad17e4b992678810
Iojioji <erfamel@hotmail.com>
2020-11-13 12:17:18 -0600

    update sendgrid config

--- api/hello/controllers/Hello.js
+++ api/hello/controllers/Hello.js
@@ -1,6 +1,14 @@
 module.exports = {
     // GET /hello
     async index(ctx) {
-        ctx.send(strapi.plugins["email"]);
+        // console.log(strapi.plugins["email"].services.email.getProviderConfig());
+        // strapi.plugins["email"].services.email.send({
+        //     to: 'erfamel@gmail.com',
+        //     from: 'will@wetheforce.com',
+        //     subject: 'Que rosho carnal',
+        //     text: 'que rosho carnal text',
+        //     html: "Esto es un html, como andas man"
+        // });
+        ctx.send("Sis");
     },
   };
