11442e4012da619c7948401f38842d9e776de6b3
Iojioji <erfamel@hotmail.com>
2020-11-13 13:08:34 -0600

    Disable sendgrid

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,16 @@
-module.exports = ({env}) => (
-    console.log("ayyy\r\n", process.env.SENDGRID_API_KEY),
-    {
-        email: {
-            provider: 'sendgrid',
-            providerOptions: {
-                apiKey: process.env.SENDGRID_API_KEY,
-            },
-            settings: {
+// module.exports = ({env}) => (
+//     console.log("ayyy\r\n", process.env.SENDGRID_API_KEY),
+//     {
+//         email: {
+//             provider: 'sendgrid',
+//             providerOptions: {
+//                 apiKey: process.env.SENDGRID_API_KEY,
+//             },
+//             settings: {
                 
-                defaultFrom: 'will@wetheforce.com',
-                defaultReplyTo: 'will@wetheforce.com',
-            },
-        },
-    }
-);
+//                 defaultFrom: 'will@wetheforce.com',
+//                 defaultReplyTo: 'will@wetheforce.com',
+//             },
+//         },
+//     }
+// );
