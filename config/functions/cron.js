3c5b9615e225b96c6143036fbee715add38aa6c3
Will Alvarez <will.alvarez@gmail.com>
2020-11-25 12:25:19 -0600

    cron every minute

--- config/functions/cron.js
+++ config/functions/cron.js
@@ -19,7 +19,7 @@ module.exports = {
   //
   // }
 
-  '0 0 16 * * *': async () => {
+  '* * * * * *': async () => {
     let today = new Date();
     console.log(`${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}`);
 
