6f1c39cf0440a782049a8c47b62340c9a5d77dfa
Will Alvarez <will.alvarez@gmail.com>
2020-11-25 18:15:58 -0600

    populate user media

---
+++ extensions/users-permissions/controllers/User.js
@@ -0,0 +1,21 @@
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+/**
+ * Retrieve a user record.
+ * @return {Object}
+ */
+async findOne(ctx, { populate } = {}) {
+  const { id } = ctx.params;
+  let data = await strapi.plugins['users-permissions'].services.user.fetch({
+    id,
+  }, populate);
+
+  if (data) {
+    data = sanitizeUser(data);
+  }
+
+  // Send 200 `ok`
+  ctx.send(data);
+},
+};
