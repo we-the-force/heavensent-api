7c3f1271ce396112f9de1dbb4cd51e9bfbc9a239
Will Alvarez <will.alvarez@gmail.com>
2020-11-25 18:20:33 -0600

    user with media change

--- extensions/users-permissions/controllers/User.js
+++ extensions/users-permissions/controllers/User.js
@@ -1,26 +1,22 @@
 const { sanitizeEntity } = require('strapi-utils');
 
 const sanitizeUser = user =>
-sanitizeEntity(user, {
-  model: strapi.query('user', 'users-permissions').model,
-});
+  sanitizeEntity(user, {
+    model: strapi.query('user', 'users-permissions').model,
+  });
 
 module.exports = {
-/**
- * Retrieve a user record.
- * @return {Object}
- */
-async findOne(ctx, { populate } = {}) {
-  const { id } = ctx.params;
-  let data = await strapi.plugins['users-permissions'].services.user.fetch({
-    id,
-  }, populate);
+  async me(ctx) {
+    const user = ctx.state.user;
 
-  if (data) {
-    data = sanitizeUser(data);
-  }
+    if (!user) {
+      return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
+    }
 
-  // Send 200 `ok`
-  ctx.send(data);
-},
+    const userQuery = await strapi.query('user', 'users-permissions');
+    const userWithMedia = userQuery.findOne({ id: ctx.state.user.id }, ['mediaField']);
+
+    const data = sanitizeUser(userWithMedia, { model: userQuery.model });
+    ctx.send(data);
+  },
 };
