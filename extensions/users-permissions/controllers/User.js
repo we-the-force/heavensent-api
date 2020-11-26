1a49b778e38956839cb6726d9f0e7bbe9242404c
Will Alvarez <will.alvarez@gmail.com>
2020-11-25 18:31:39 -0600

    new test for user

--- extensions/users-permissions/controllers/User.js
+++ extensions/users-permissions/controllers/User.js
@@ -1,22 +1,40 @@
+// const { sanitizeEntity } = require('strapi-utils');
+
+// const sanitizeUser = user =>
+//   sanitizeEntity(user, {
+//     model: strapi.query('user', 'users-permissions').model,
+//   });
+
+// module.exports = {
+//   async findOne(ctx) {
+//     // const user = ctx.state.user;
+//     const { id } = ctx.params;
+
+//     if (!user) {
+//       return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
+//     }
+
+//     const userQuery = await strapi.query('user', 'users-permissions');
+//     const userWithMedia = await userQuery.findOne({ id: id });
+
+//     const data = sanitizeUser(userWithMedia, { model: userQuery.model });
+//     ctx.send(data);
+//   },
+// };
+
 const { sanitizeEntity } = require('strapi-utils');
 
-const sanitizeUser = user =>
-  sanitizeEntity(user, {
-    model: strapi.query('user', 'users-permissions').model,
-  });
-
 module.exports = {
-  async me(ctx) {
-    const user = ctx.state.user;
+  /**
+   * Retrieve a record.
+   *
+   * @return {Object}
+   */
 
-    if (!user) {
-      return ctx.badRequest(null, [{ messages: [{ id: 'No authorization header was found' }] }]);
-    }
+  async findOne(ctx) {
+    const { id } = ctx.params;
 
-    const userQuery = await strapi.query('user', 'users-permissions');
-    const userWithMedia = userQuery.findOne({ id: ctx.state.user.id }, ['profilePicture']);
-
-    const data = sanitizeUser(userWithMedia, { model: userQuery.model });
-    ctx.send(data);
+    const entity = await strapi.services.user.findOne({ id });
+    return sanitizeEntity(entity, { model: strapi.models.user });
   },
 };
